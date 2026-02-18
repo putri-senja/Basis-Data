037abd950e585935456d1762aecb5f79e767b156
Putri Senja <ptrsenjana30@gmail.com>
2026-02-18 18:06:41 +0700

    Add files via upload

--- untidar_backup.sql
+++ untidar_backup.sql
@@ -14,6 +14,78 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dosen`
+--
+
+DROP TABLE IF EXISTS `dosen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dosen` (
+  `NID` char(10) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jenis_kelamin` enum('L','P') DEFAULT NULL,
+  PRIMARY KEY (`NID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dosen`
+--
+
+LOCK TABLES `dosen` WRITE;
+/*!40000 ALTER TABLE `dosen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dosen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+DROP TABLE IF EXISTS `mahasiswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mahasiswa` (
+  `NPM` char(10) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `tempat_lahir` varchar(20) NOT NULL,
+  `jenis_kelamin` enum('L','P') DEFAULT NULL,
+  PRIMARY KEY (`NPM`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+LOCK TABLES `mahasiswa` WRITE;
+/*!40000 ALTER TABLE `mahasiswa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mahasiswa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `matakuliah`
+--
+
+DROP TABLE IF EXISTS `matakuliah`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `matakuliah` (
+  `nama_matkul` varchar(20) NOT NULL,
+  `jadwal` date NOT NULL,
+  `nama_dosen` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `matakuliah`
+--
+
+LOCK TABLES `matakuliah` WRITE;
+/*!40000 ALTER TABLE `matakuliah` DISABLE KEYS */;
+/*!40000 ALTER TABLE `matakuliah` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +96,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-02-18 10:45:33
+-- Dump completed on 2026-02-18 18:05:04
